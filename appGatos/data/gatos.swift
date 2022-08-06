e5e2d20d82e2d1a0b4afbe0c60575c55cded254c
Luis Mindiola <lmindiola@lya-electronic.com>
2022-08-06 09:06:50 -0500

    implementada estructura

---
+++ appGatos/data/gatos.swift
@@ -0,0 +1,22 @@
+//
+//  gatos.swift
+//  appGatos
+//
+//  Created by Luis Mindiola Gonzalez on 6/08/22.
+//
+
+import Foundation
+
+
+struct Gatos {
+    let gato : Gato
+}
+
+struct Gato {
+    let breedName: String?
+    let origin: String?
+    let affectionLevel: Int?
+    let inteligent: Int?
+    let image: String?
+    
+}
