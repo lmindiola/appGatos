ebd2a06becfa28de72858fdb5d12c5cc24cfe442
Luis Mindiola <lmindiola@lya-electronic.com>
2022-08-06 08:50:38 -0500

    Initial Commit

---
+++ appGatos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appGatos
+//
+//  Created by Luis Mindiola Gonzalez on 6/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
